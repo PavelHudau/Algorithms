1726d0b3afb3d20c69b790b20e8276bfbafcd0ed
Pavel Hudau <phudau@bloomberg.net>
2020-12-23 18:12:57 -0500

    add Permutation

--- Percolation/src/main/java/com/pavelhudau/queues/Permutation.java
+++ Percolation/src/main/java/com/pavelhudau/queues/Permutation.java
@@ -1,7 +1,23 @@
 package com.pavelhudau.queues;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rangQ = new RandomizedQueue<>();
+        while (true) {
+            String input = StdIn.readString();
+            if (input == null) {
+                break;
+            }
+            rangQ.enqueue(input);
+        }
 
+        while (k > 0 && !rangQ.isEmpty()) {
+            StdOut.print(rangQ.dequeue());
+            k--;
+        }
     }
 }
