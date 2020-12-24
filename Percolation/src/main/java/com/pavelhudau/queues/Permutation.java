eb232e644bf43cc6353f01a9abf710f976ec84b6
Pavel Hudau <pavelhudau@Pavels-MacBook-Air.local>
2020-12-23 20:22:33 -0500

    fix Permutation

--- Percolation/src/main/java/com/pavelhudau/queues/Permutation.java
+++ Percolation/src/main/java/com/pavelhudau/queues/Permutation.java
@@ -7,16 +7,13 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> rangQ = new RandomizedQueue<>();
-        while (true) {
+        while (!StdIn.isEmpty()) {
             String input = StdIn.readString();
-            if (input == null) {
-                break;
-            }
             rangQ.enqueue(input);
         }
 
         while (k > 0 && !rangQ.isEmpty()) {
-            StdOut.print(rangQ.dequeue());
+            StdOut.println(rangQ.dequeue());
             k--;
         }
     }
