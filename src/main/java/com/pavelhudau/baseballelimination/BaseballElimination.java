bd2876dc305df2ecf6e9b4475e2d3aebaf688cee
Pavel Hudau <8116717+PavelHudau@users.noreply.github.com>
2021-04-01 21:52:57 -0400

    Baseball elimination problem (#13)
    * baseball elimination

---
+++ src/main/java/com/pavelhudau/baseballelimination/BaseballElimination.java
@@ -0,0 +1,277 @@
+package com.pavelhudau.baseballelimination;
+
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+
+import java.util.ArrayList;
+
+public class BaseballElimination {
+    private static final int SOURCE_VERTEX = 0;
+    private static final int TARGET_VERTEX = 1;
+
+    private final int numOfTeams;
+    private final ST<String, Integer> teamsToIdx;
+    private final String[] teams;
+    private final int[] wins;
+    private final int[] loss;
+    private final int[] left;
+    private final int[][] games;
+
+    private int lastTeamIdx = -1;
+    private boolean lastTeamIsEliminated;
+    private ArrayList<String> lastTeamCertificateOfElimination = null;
+
+    /**
+     * Create a baseball division from given filename in format specified below.
+     * <p>
+     * The input format is the number of teams in the division n followed by one line for each team. Each line contains
+     * the team name (with no internal whitespace characters), the number of wins, the number of losses, the number of
+     * remaining games, and the number of remaining games against each team in the division.
+     * <p/>
+     * <p>
+     * team         wins loss left   Atl Phi NY  Mon
+     * <p>
+     * ------------------------------------------------
+     * <p>
+     * Atlanta       83   71    8     -   1   6   1
+     * <p>
+     * Philadelphia  80   79    3     1   -   0   2
+     * <p>
+     * New York      78   78    6     6   0   -   0
+     * <p>
+     * Montreal      77   82    3     1   2   0   -
+     *
+     * @param filename name of a file.
+     */
+    public BaseballElimination(String filename) {
+        In in = new In(filename);
+        this.numOfTeams = in.readInt();
+        this.wins = new int[this.numOfTeams];
+        this.loss = new int[this.numOfTeams];
+        this.left = new int[this.numOfTeams];
+        this.games = new int[this.numOfTeams][this.numOfTeams];
+        this.teamsToIdx = new ST<>();
+        this.teams = new String[this.numOfTeams];
+        for (int teamIdx = 0; teamIdx < numOfTeams; teamIdx++) {
+            String teamName = in.readString();
+            this.teamsToIdx.put(teamName, teamIdx);
+            this.teams[teamIdx] = teamName;
+            this.wins[teamIdx] = in.readInt();
+            this.loss[teamIdx] = in.readInt();
+            this.left[teamIdx] = in.readInt();
+            this.games[teamIdx] = new int[this.numOfTeams];
+            for (int j = 0; j < this.numOfTeams; j++) {
+                this.games[teamIdx][j] = in.readInt();
+            }
+        }
+    }
+
+    /**
+     * Number of teams.
+     *
+     * @return number of teams.
+     */
+    public int numberOfTeams() {
+        return this.numOfTeams;
+    }
+
+    /**
+     * All teams.
+     *
+     * @return All teams names.
+     */
+    public Iterable<String> teams() {
+        return this.teamsToIdx.keys();
+    }
+
+    /**
+     * Number of wins for given team.
+     *
+     * @param team Team name.
+     * @return Number of wins for given team.
+     */
+    public int wins(String team) {
+        return this.wins[this.resolveTeam(team)];
+    }
+
+
+    /**
+     * Number of losses for given team.
+     *
+     * @param team Team name.
+     * @return Number of losses for given team.
+     */
+    public int losses(String team) {
+        return this.loss[this.resolveTeam(team)];
+    }
+
+    /**
+     * Number of remaining games for given team.
+     *
+     * @param team Team name.
+     * @return Number of remaining games for given team.
+     */
+    public int remaining(String team) {
+        return this.left[this.resolveTeam(team)];
+    }
+
+    /**
+     * Number of remaining games between team1 and team2.
+     *
+     * @param team1 Team name.
+     * @param team2 Other team name.
+     * @return Number of remaining games between team1 and team2.
+     */
+    public int against(String team1, String team2) {
+        return this.games[this.resolveTeam(team1)][this.resolveTeam(team2)];
+    }
+
+    /**
+     * Determine whether a given team is eliminated?
+     *
+     * @param team Team name.
+     * @return True if a given team is eliminated, else False.
+     */
+    public boolean isEliminated(String team) {
+        int teamIdx = this.resolveTeam(team);
+        if (this.lastTeamIdx != teamIdx) {
+            this.calculateForTeam(teamIdx);
+        }
+
+        return this.lastTeamIsEliminated;
+    }
+
+    /**
+     * Subset R of teams that eliminates given team.
+     *
+     * @param team Team name.
+     * @return Collection of teams names that eliminates given team; null if not eliminated.
+     */
+    public Iterable<String> certificateOfElimination(String team) {
+        int teamX = this.resolveTeam(team);
+        if (teamX != this.lastTeamIdx) {
+            this.calculateForTeam(teamX);
+        }
+
+        return this.lastTeamCertificateOfElimination;
+    }
+
+    private void calculateForTeam(int teamX) {
+        this.lastTeamIdx = teamX;
+
+        ArrayList<String> certificateOfElimination = this.getTrivialCertificateOfElimination(teamX);
+        if (certificateOfElimination.isEmpty()) {
+            certificateOfElimination = this.getNominalCertificateOfElimination(teamX);
+        }
+
+        if (certificateOfElimination.isEmpty()) {
+            this.lastTeamCertificateOfElimination = null;
+            this.lastTeamIsEliminated = false;
+        } else {
+            this.lastTeamCertificateOfElimination = certificateOfElimination;
+            this.lastTeamIsEliminated = true;
+        }
+    }
+
+    private int resolveTeam(String teamName) {
+        if (teamName == null) {
+            throw new IllegalArgumentException("team can not be null");
+        }
+        if (this.teamsToIdx.contains(teamName)) {
+            return this.teamsToIdx.get(teamName);
+        } else {
+            throw new IllegalArgumentException("Unknown team " + teamName);
+        }
+    }
+
+    private ArrayList<String> getTrivialCertificateOfElimination(int teamX) {
+        ArrayList<String> certificateOfElimination = new ArrayList<>();
+        int teamMaxPossibleWins = this.wins[teamX] + this.left[teamX];
+
+        for (int teamI = 0; teamI < this.numOfTeams; teamI++) {
+            if (teamI != teamX && this.wins[teamI] > teamMaxPossibleWins) {
+                certificateOfElimination.add(this.teams[teamI]);
+            }
+        }
+
+        return certificateOfElimination;
+    }
+
+    private ArrayList<String> getNominalCertificateOfElimination(int teamX) {
+        FlowNetwork network = createFlowNetwork(teamX);
+        FordFulkerson fordFulkerson = new FordFulkerson(network, SOURCE_VERTEX, TARGET_VERTEX);
+        ArrayList<String> certificateOfElimination = new ArrayList<>();
+
+        for (int teamI = 0; teamI < this.numOfTeams; teamI++) {
+            if (teamI != teamX) {
+                if (fordFulkerson.inCut(teamToVertex(teamI, teamX))) {
+                    // If team i is in a min cut then it means that team i has still games to play (there is still residual
+                    // value of one of team's i game edges) but the team i already won more games than teamX can possibly do
+                    // (Edge [team i -> target] has no residual value left).
+                    certificateOfElimination.add(this.teams[teamI]);
+                }
+            }
+        }
+
+        return certificateOfElimination;
+    }
+
+    private FlowNetwork createFlowNetwork(int teamX) {
+        // Vertices are assigned the following way:
+        // 0 => Source
+        // 1 => Target
+        // 2 .. N - 2 => Teams Vertices where
+        //   N - 1 => teams less the team we are checking elimination for
+        //   N - 2 => because vertices are 0-based.
+        // N - 1 .. END => Games vertices. We start game vertices right after team vertices. We do not need to come
+        // back to the game vertices, therefore we do not care what indices they will get,
+        // as long as they are in the range and valid.
+        int nextGamesVertex = 2 + this.numOfTeams - 1;
+        FlowNetwork network = new FlowNetwork(nextGamesVertex + this.totalGameVertices());
+        int maxPossibleWinsOfTeamX = this.wins[teamX] + this.left[teamX];
+
+        for (int teamI = 0; teamI < this.numOfTeams; teamI++) {
+            if (teamI == teamX) {
+                continue;
+            }
+
+            // Below loop adds edges corresponding to all possible games between all teams except for team x
+            for (int teamJ = teamI + 1; teamJ < this.numOfTeams; teamJ++) {
+                if (teamJ == teamX) {
+                    continue;
+                }
+
+                // Connect source and game vertices.
+                network.addEdge(new FlowEdge(SOURCE_VERTEX, nextGamesVertex, this.games[teamI][teamJ]));
+                // Connect game vertex with team vertices.
+                network.addEdge(new FlowEdge(nextGamesVertex, teamToVertex(teamI, teamX), Double.POSITIVE_INFINITY));
+                network.addEdge(new FlowEdge(nextGamesVertex, teamToVertex(teamJ, teamX), Double.POSITIVE_INFINITY));
+                nextGamesVertex++;
+            }
+
+            // Connect team vertex to target vertex.
+            // We want to know if there is some way of completing all the games so that team x ends up winning at least
+            // as many games as team i. Since team x can win as many as this.wins[x] + this.left[x] games, we prevent
+            // team i from winning more than that many games in total, by including an edge from team vertex i to the
+            // target vertex with capacity  this.wins[x] + this.left[x] - this.win[i].
+            network.addEdge(new FlowEdge(
+                    teamToVertex(teamI, teamX),
+                    TARGET_VERTEX,
+                    Math.max(maxPossibleWinsOfTeamX - this.wins[teamI], 0)));
+        }
+
+        return network;
+    }
+
+    private static int teamToVertex(int team, int teamX) {
+
+        return team < teamX ? team + 2 : team + 1;
+    }
+
+    private int totalGameVertices() {
+        return (this.numOfTeams * this.numOfTeams - this.numOfTeams) / 2 - (this.numOfTeams - 1);
+    }
+}
---
+++ src/test/java/com/pavelhudau/baseballelimination/TestBaseballElimination.java
@@ -0,0 +1,283 @@
+package com.pavelhudau.baseballelimination;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBaseballElimination {
+    private final static String fileFourTeams = "src/main/resources/teams4.txt";
+    private final static String fileFiveTeams = "src/main/resources/teams5.txt";
+
+    @ParameterizedTest
+    @CsvSource({
+            fileFourTeams + ", 4",
+            fileFiveTeams + ", 5",
+    })
+    void testNumberOfTeams(String file, int expectedNumberOfTeams) {
+        BaseballElimination bm = new BaseballElimination(file);
+        assertEquals(expectedNumberOfTeams, bm.numberOfTeams());
+    }
+
+    @Test
+    void testTeams() {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        Set<String> expectedTeams = new HashSet<>();
+        expectedTeams.add("Atlanta");
+        expectedTeams.add("Philadelphia");
+        expectedTeams.add("New_York");
+        expectedTeams.add("Montreal");
+        int teamsCount = 0;
+        // WHEN
+        Iterable<String> teams = bm.teams();
+        // THEN
+        for (String team : teams) {
+            teamsCount++;
+            assertTrue(expectedTeams.contains(team));
+        }
+        assertEquals(4, teamsCount);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            fileFourTeams + ", Atlanta, 83",
+            fileFourTeams + ", Philadelphia, 80",
+            fileFourTeams + ", New_York, 78",
+            fileFourTeams + ", Montreal, 77",
+            fileFiveTeams + ", New_York, 75",
+            fileFiveTeams + ", Baltimore, 71",
+            fileFiveTeams + ", Boston, 69",
+            fileFiveTeams + ", Toronto, 63",
+            fileFiveTeams + ", Detroit, 49",
+    })
+    void testWinsWhenInputIsCorrect(String file, String team, int expectedWins) {
+        BaseballElimination bm = new BaseballElimination(file);
+        assertEquals(expectedWins, bm.wins(team));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "NonExistingTeam"})
+    void testWinsWhenTeamNameIsInvalid(String team) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.wins(team));
+    }
+
+    @Test
+    void testWinsWhenTeamNameIsNull() {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.wins(null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            fileFourTeams + ", Atlanta, 71",
+            fileFourTeams + ", Philadelphia, 79",
+            fileFourTeams + ", New_York, 78",
+            fileFourTeams + ", Montreal, 82",
+            fileFiveTeams + ", New_York, 59",
+            fileFiveTeams + ", Baltimore, 63",
+            fileFiveTeams + ", Boston, 66",
+            fileFiveTeams + ", Toronto, 72",
+            fileFiveTeams + ", Detroit, 86",
+    })
+    void testLossesWhenInputIsCorrect(String file, String team, int expectedWins) {
+        BaseballElimination bm = new BaseballElimination(file);
+        assertEquals(expectedWins, bm.losses(team));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "NonExistingTeam"})
+    void testLossesWhenTeamNameIsInvalid(String team) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.losses(team));
+    }
+
+    @Test
+    void testLossesWhenTeamNameIsNull() {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.losses(null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            fileFourTeams + ", Atlanta, 8",
+            fileFourTeams + ", Philadelphia, 3",
+            fileFourTeams + ", New_York, 6",
+            fileFourTeams + ", Montreal, 3",
+            fileFiveTeams + ", New_York, 28",
+            fileFiveTeams + ", Baltimore, 28",
+            fileFiveTeams + ", Boston, 27",
+            fileFiveTeams + ", Toronto, 27",
+            fileFiveTeams + ", Detroit, 27",
+    })
+    void testRemainingWhenInputIsCorrect(String file, String team, int expectedWins) {
+        BaseballElimination bm = new BaseballElimination(file);
+        assertEquals(expectedWins, bm.remaining(team));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "NonExistingTeam"})
+    void testRemainingWhenTeamNameIsInvalid(String team) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.remaining(team));
+    }
+
+    @Test
+    void testRemainingWhenTeamNameIsNull() {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.remaining(null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Atlanta , Atlanta, 0",
+            "Philadelphia, Philadelphia, 0",
+            "New_York, New_York, 0",
+            "Montreal, Montreal, 0",
+            "Atlanta , Philadelphia, 1",
+            "Atlanta, New_York, 6",
+            "Atlanta, Montreal, 1",
+            "New_York, New_York, 0",
+            "New_York, Atlanta, 6",
+            "New_York, New_York, 0"
+    })
+    void testAgainstWhenInputIsCorrect(String team1, String team2, int expectedWins) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertEquals(expectedWins, bm.against(team1, team2));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "NonExistingTeam"})
+    void testAgainstWhenTeamNameIsInvalid(String invalidTeam) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.against("Philadelphia", invalidTeam));
+        assertThrows(IllegalArgumentException.class, () -> bm.against(invalidTeam, "Philadelphia"));
+    }
+
+    @Test
+    void testAgainstWhenTeamNameIsNull() {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertThrows(IllegalArgumentException.class, () -> bm.against("Philadelphia", null));
+        assertThrows(IllegalArgumentException.class, () -> bm.against(null, "Philadelphia"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Atlanta , false",
+            "Philadelphia,true",
+            "New_York, false",
+            "Montreal, true",
+    })
+    void testIsEliminatedWithFourTeams(String team, boolean expectedIsEliminates) {
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        assertEquals(expectedIsEliminates, bm.isEliminated(team));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "New_York , false",
+            "Baltimore,false",
+            "Boston, false",
+            "Toronto, false",
+            "Detroit, true",
+    })
+    void testIsEliminatedWithFiveTeams(String team, boolean expectedIsEliminates) {
+        BaseballElimination bm = new BaseballElimination(fileFiveTeams);
+        assertEquals(expectedIsEliminates, bm.isEliminated(team));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Atlanta",
+            "New_York",
+    })
+    void testCertificateOfEliminationWithFourTeamsWhenNotEliminated(String team) {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        // WHEN
+        Iterable<String> certificateOfElimination = bm.certificateOfElimination(team);
+        // THEN
+        assertNull(certificateOfElimination);
+    }
+
+    @Test
+    void testCertificateOfEliminationWithFourTeamsWhenPhiladelphiaIsEliminated() {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        HashSet<String> expectedCertificateOfElimination = new HashSet<>();
+        expectedCertificateOfElimination.add("Atlanta");
+        expectedCertificateOfElimination.add("New_York");
+        // WHEN
+        Iterable<String> certificateOfElimination = bm.certificateOfElimination("Philadelphia");
+        //THEN
+        List<String> certificateOfEliminationActual = StreamSupport
+                .stream(certificateOfElimination.spliterator(), false)
+                .collect(Collectors.toList());
+        assertEquals(expectedCertificateOfElimination.size(), certificateOfEliminationActual.size());
+        for (String actual : certificateOfEliminationActual) {
+            assertTrue(expectedCertificateOfElimination.contains(actual));
+        }
+    }
+
+    @Test
+    void testCertificateOfEliminationWithFourTeamsWhenMontrealIsEliminated() {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFourTeams);
+        HashSet<String> expectedCertificateOfElimination = new HashSet<>();
+        expectedCertificateOfElimination.add("Atlanta");
+        // WHEN
+        Iterable<String> certificateOfElimination = bm.certificateOfElimination("Montreal");
+        //THEN
+        List<String> certificateOfEliminationActual = StreamSupport
+                .stream(certificateOfElimination.spliterator(), false)
+                .collect(Collectors.toList());
+        assertEquals(expectedCertificateOfElimination.size(), certificateOfEliminationActual.size());
+        for (String actual : certificateOfEliminationActual) {
+            assertTrue(expectedCertificateOfElimination.contains(actual));
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "New_York",
+            "Baltimore",
+            "Boston",
+            "Toronto"
+    })
+    void testCertificateOfEliminationWithFiveTeamsWhenNotEliminated(String team) {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFiveTeams);
+        // WHEN
+        Iterable<String> certificateOfElimination = bm.certificateOfElimination(team);
+        // THEN
+        assertNull(certificateOfElimination);
+    }
+
+    @Test
+    void testCertificateOfEliminationWithFiveTeamsWhenDetroitIsEliminated() {
+        // GIVEN
+        BaseballElimination bm = new BaseballElimination(fileFiveTeams);
+        HashSet<String> expectedCertificateOfElimination = new HashSet<>();
+        expectedCertificateOfElimination.add("New_York");
+        expectedCertificateOfElimination.add("Boston");
+        expectedCertificateOfElimination.add("Baltimore");
+        expectedCertificateOfElimination.add("Toronto");
+        // WHEN
+        Iterable<String> certificateOfElimination = bm.certificateOfElimination("Detroit");
+        //THEN
+        List<String> certificateOfEliminationActual = StreamSupport
+                .stream(certificateOfElimination.spliterator(), false)
+                .collect(Collectors.toList());
+        assertEquals(expectedCertificateOfElimination.size(), certificateOfEliminationActual.size());
+        for (String actual : certificateOfEliminationActual) {
+            assertTrue(expectedCertificateOfElimination.contains(actual));
+        }
+    }
+}
