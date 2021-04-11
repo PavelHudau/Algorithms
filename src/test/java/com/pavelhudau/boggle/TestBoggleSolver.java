df039249c4b21f11b398ff34317d97e2054016de
Pavel Hudau <paylikn@gmail.com>
2021-04-11 17:22:15 -0400

    improve tests speed

--- src/test/java/com/pavelhudau/boggle/TestBoggleSolver.java
+++ src/test/java/com/pavelhudau/boggle/TestBoggleSolver.java
@@ -3,11 +3,13 @@ package com.pavelhudau.boggle;
 import edu.princeton.cs.algs4.In;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBoggleSolver {
     private static final String DICTIONARY_ALGS_4_TXT = "dictionary-algs4.txt";
+    private static BoggleSolver dictAlg4Solver;
 
     @ParameterizedTest
     @CsvSource({
@@ -22,9 +24,35 @@ public class TestBoggleSolver {
             "ANTISYMMETRIC, 11"
 
     })
-    void testScore(String word, int expectedScore) {
-        BoggleSolver solver = new BoggleSolver(readDictionary(DICTIONARY_ALGS_4_TXT));
+    void testScoreWhenWordExists(String word, int expectedScore) {
+        BoggleSolver solver = getDictAlg4Solver();
         assertEquals(expectedScore, solver.scoreOf(word));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "z",
+            "al",
+            "act",
+            "also",
+            "amino",
+            "analog",
+            "analogy",
+            "analyses",
+            "antisymmetric"
+    })
+    void testScoreWhenWordDoesNotExist(String word) {
+        int expectedScore = 0;
+        BoggleSolver solver = getDictAlg4Solver();
+        assertEquals(expectedScore, solver.scoreOf(word));
+    }
+
+    private static BoggleSolver getDictAlg4Solver() {
+        if (dictAlg4Solver == null) {
+            dictAlg4Solver = new BoggleSolver(readDictionary(DICTIONARY_ALGS_4_TXT));
+        }
+
+        return dictAlg4Solver;
     }
 
     private static String[] readDictionary(String fileName) {
