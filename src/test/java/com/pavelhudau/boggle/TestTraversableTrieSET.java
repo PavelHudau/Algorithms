d3153ea1ede00ba4481c623d35cf03fcf7094084
Pavel Hudau <paylikn@gmail.com>
2021-04-17 20:06:24 -0400

    add trest for a huge dictionary

--- src/test/java/com/pavelhudau/boggle/TestTraversableTrieSET.java
+++ src/test/java/com/pavelhudau/boggle/TestTraversableTrieSET.java
@@ -1,5 +1,6 @@
 package com.pavelhudau.boggle;
 
+import edu.princeton.cs.algs4.In;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,6 +9,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestTraversableTrieSET {
+    private static final String RESOURCES_PATH = "src/main/resources/boggle/";
+    private static final String DICTIONARY_ZINGARELLI_2005 = "dictionary-zingarelli2005.txt";
+
     @Test
     void testGetRootWhenNoWordsAdded() {
         TraversableTrieSET trieSet = new TraversableTrieSET();
@@ -51,5 +55,29 @@ public class TestTraversableTrieSET {
         assertFalse(trieSet.contains("Z"));
         assertFalse(trieSet.contains("T"));
         assertFalse(trieSet.contains("DD"));
+    }
+
+    @Test
+    void testAddAndContainsWithLargeDictionary() {
+        // GIVEN
+        TraversableTrieSET trieSet = new TraversableTrieSET();
+
+        // WHEN
+        for (String word : readDictionary(DICTIONARY_ZINGARELLI_2005)) {
+            trieSet.add(word);
+        }
+
+        // THEN
+        assertTrue(trieSet.contains("ZWINGLISTI"));
+        assertTrue(trieSet.contains("ZZZ"));
+        assertTrue(trieSet.contains("ABACA"));
+        assertFalse(trieSet.contains("AAA"));
+        assertFalse(trieSet.contains("ZZZZ"));
+
+    }
+
+    private static String[] readDictionary(String fileName) {
+        In in = new In(RESOURCES_PATH + fileName);
+        return in.readAllStrings();
     }
 }
